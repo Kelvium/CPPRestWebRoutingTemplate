39fd531c38a55e4e81abe3656b028fc7b2106d54
Kelvium <ufh8945@gmail.com>
2021-02-27 08:24:26 +0300

    Fix GET request handler

--- src/handlers/get.cpp
+++ src/handlers/get.cpp
@@ -8,7 +8,7 @@
 namespace App::handlers {
     void getHandler(const web::http::http_request& request) {
         for(const auto& route : routes::routes) {
-            if(route.url == request.request_uri())
+            if(route.url == request.request_uri() && request.method() == web::http::methods::GET)
                 route.handler(request);
             else
                 request.reply(web::http::status_codes::NotFound, "404 - Not Found");
