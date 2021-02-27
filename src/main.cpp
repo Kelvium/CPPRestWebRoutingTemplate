52208e967c3702ec8ac566cab0f95f277f22552c
Kelvium <ufh8945@gmail.com>
2021-02-27 08:38:38 +0300

    Use this all in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+//
+// Created by kelvium on 27.02.2021.
+//
+
+#include "application.hpp"
+#include "handlers/get.hpp" // we use only get handler. you may use other handlers
+#include <iostream>
+
 int main() {
-    return 0;
+    std::vector<App::MethodHandler> handlers = {
+            {web::http::methods::GET, App::handlers::getHandler}
+    };
+    App::Application app;
+    app.initialize("http://127.0.0.1/", handlers);
+    try {
+        app.listen();
+    } catch (const std::exception &exception) {
+        std::cerr << exception.what() << std::endl;
+    }
 }
