2125edb51ee5f59643decb256da0a46f4a2bba6a
Kelvium <ufh8945@gmail.com>
2021-02-27 08:39:20 +0300

    Fix status code in index.cpp

--- src/routes/index.cpp
+++ src/routes/index.cpp
@@ -6,6 +6,6 @@
 
 namespace App::routes {
     void indexRoute(const web::http::http_request &request) {
-        request.reply(web::http::status_codes::Ok, "Hi! My template works! It's good i think.");
+        request.reply(web::http::status_codes::OK, "Hi! My template works! It's good i think.");
     }
 }
