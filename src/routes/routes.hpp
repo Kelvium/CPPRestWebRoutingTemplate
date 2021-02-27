abbf71072837f591620d33418cfa19a46d80c2dd
Kelvium <ufh8945@gmail.com>
2021-02-27 08:20:01 +0300

    Implement routes list

---
+++ src/routes/routes.hpp
@@ -0,0 +1,15 @@
+//
+// Created by kelvium on 27.02.2021.
+//
+
+#pragma once
+
+#include <vector>
+#include "route.hpp"
+
+namespace App::routes {
+    // To add your routes you need to insert them here!
+    const std::vector<Route> routes {
+
+    }
+}
