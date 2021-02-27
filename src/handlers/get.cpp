9143bfc2f77c18ab7bdc5df330d05d2057042583
Kelvium <ufh8945@gmail.com>
2021-02-27 08:23:44 +0300

    Implement GET request handler

---
+++ src/handlers/get.cpp
@@ -0,0 +1,17 @@
+//
+// Created by kelvium on 27.02.2021.
+//
+
+#include "get.hpp"
+#include "../routes/routes.hpp"
+
+namespace App::handlers {
+    void getHandler(const web::http::http_request& request) {
+        for(const auto& route : routes::routes) {
+            if(route.url == request.request_uri())
+                route.handler(request);
+            else
+                request.reply(web::http::status_codes::NotFound, "404 - Not Found");
+        }
+    }
+}
